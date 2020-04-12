2f52fa5c2038915f7cd522de0ca28ece57518d24
Justyna Skrajna <justyna.skrajna@aliso.pl>
2020-04-12 23:39:24 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/main.js',
+  output: {
+    filename: 'script.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin(
+      {
+        template: './src/index.html',
+        filename: './index.html'
+      }
+    )
+  ]
+};
